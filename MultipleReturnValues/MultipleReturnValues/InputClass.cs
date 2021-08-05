7f56df07feffd3bed708072108fa1ca480822510
smomin156 <63006524+smomin156@users.noreply.github.com>
2021-08-05 18:46:58 +0530

    Add files via upload

---
+++ MultipleReturnValues/MultipleReturnValues/InputClass.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleReturnValues
+{
+    static class InputClass
+    {
+        //using out 
+        public static void FindMinMax1(int[] arr, out int min, out int max)
+        {
+            min = arr[0];
+            max = arr[0];
+            foreach (int count in arr)
+            {
+                if (count <= min)
+                {
+                    min = count;
+                }
+                if (count >= max)
+                {
+                    max = count;
+                }
+            }
+
+        }
+        //using ref
+        public static void FindMinMax2(int[] arr, ref int min, ref int max)
+        {
+            
+            foreach (int count in arr)
+            {
+                if (count <= min)
+                {
+                    min = count;
+                }
+                if (count >= max)
+                {
+                    max = count;
+                }
+            }
+
+        }
+
+        //using array
+        public static int[] FindMinMax3(int[] arr)
+        {
+            int[] minMax=new int[2];
+            minMax[0] = arr[0];
+            minMax[1] = arr[1];
+            foreach (int count in arr)
+            {
+                if (count <=minMax[0])
+                {
+                    minMax[0] = count;
+                }
+                if (count >= minMax[1])
+                {
+                    minMax[1] = count;
+                }
+            }
+            return minMax;
+        }
+        //returning struct
+        
+        public static MinMax FindMinMax4(int[] arr)
+        {
+            MinMax value = new MinMax();
+            value.min = arr[0];
+            value.max = arr[1];
+            foreach (int count in arr)
+            {
+                if (count <= value.min)
+                {
+                    value.min = count;
+                }
+                if (count >= value.max)
+                {
+                    value.max = count;
+                }
+            }
+            return value;
+        }
+        //using tuple
+        public static Tuple<int,int> FindMaxMin5(int[] arr)
+        {
+            int min = arr[0];
+            int max = arr[1];
+            foreach (int count in arr)
+            {
+                if (count <=min)
+                {
+                    min = count;
+                }
+                if (count >= max)
+                {
+                    max = count;
+                }
+            }
+            return new Tuple<int, int>(min,max);
+        }
+        //using object as return
+        public static MinMax1 FindMinMax6(int[] arr)
+        {
+            MinMax1 value = new MinMax1();
+            value.Min = arr[0];
+            value.Max = arr[1];
+            foreach (int count in arr)
+            {
+                if (count <= value.Min)
+                {
+                    value.Min = count;
+                }
+                if (count >= value.Max)
+                {
+                    value.Max = count;
+                }
+            }
+            return value;
+        }
+
+    }
+    public struct MinMax
+    {
+        public int min;
+        public int max;
+    }
+
+    public class MinMax1
+    {
+        int min;
+        int max;
+        public int Min { get { return min; } set { min = value; } }
+        public int Max { get { return max; } set {max=value; } }
+    }
+}
---
+++ MultipleReturnValues/MultipleReturnValues/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleReturnValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 20, 40, 30, 5 };
+            int min1,max1;
+            //using out
+            InputClass.FindMinMax1(arr,out min1,out max1);
+            Console.WriteLine("---Min value:{0}  Max value:{1}---Using out", min1, max1);
+
+            //using ref
+            int min2 = arr[0];
+            int max2 = arr[0];
+            InputClass.FindMinMax2(arr, ref min2, ref max2);
+            Console.WriteLine("---Min value:{0}  Max value:{1}---Using ref", min2, max2);
+
+            //using array
+            int[] minMax=InputClass.FindMinMax3(arr);
+            Console.WriteLine("---Min value:{0}  Max value:{1}---using array", minMax[0], minMax[1]);
+
+            //using struct
+            MinMax value = InputClass.FindMinMax4(arr);
+            Console.WriteLine("---Min value:{0}  Max value:{1}---using struct", value.min, value.max);
+
+            //using tuple
+            var tupVar = InputClass.FindMaxMin5(arr);
+            Console.WriteLine("---Min value:{0}  Max value:{1}---using tuple", tupVar.Item1,tupVar.Item2 );
+            
+            //using object
+            MinMax1 obj = InputClass.FindMinMax6(arr);
+            Console.WriteLine("---Min value:{0}  Max value:{1}---using object", obj.Min, obj.Max);
+            Console.ReadLine();
+        }
+
+    }
+}
